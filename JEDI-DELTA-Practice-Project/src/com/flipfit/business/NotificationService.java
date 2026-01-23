eba0fe871892326b9cbb50dada7d0780e6cfdff9
Gracy Gulati <124993594+gracygulati7@users.noreply.github.com>
2026-01-23 15:42:05 +0530

    NotificationService

--- JEDI-DELTA-Practice-Project/src/com/flipfit/business/NotificationService.java
+++ JEDI-DELTA-Practice-Project/src/com/flipfit/business/NotificationService.java
@@ -1,1 +1,8 @@
 package com.flipfit.business;
+
+public interface NotificationService{
+    public void sendBookingConfirmation(int userId, int slotId);
+
+    public void sendWaitlistPromotion(int userId, int slotId);
+    
+}
